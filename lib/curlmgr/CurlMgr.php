a00cca32afabcb99a5859d041b0c27a85db01fd1
karl <fares_k@etna-alternance.net>
2018-05-16 16:39:43 +0100

    update

--- lib/curlmgr/CurlMgr.php
+++ lib/curlmgr/CurlMgr.php
@@ -39,7 +39,8 @@ class CurlMgr {
             $this->headers = array_merge($this->headers, $heads);
         }
         elseif($heads){
-            $this->headers[] = $heads;
+            $heads = explode(':', $heads);
+            $this->headers[$heads[0]] = $heads[1];
         }
         else{
             $this->headers = false;
@@ -73,15 +74,15 @@ class CurlMgr {
         if(is_array($data)){
             if ($asJson) {
                 $data = json_encode($data);
-                $this->headers[] = "Content-Type: application/json";
+                $this->headers["Content-Type"] = "application/json";
             }
             else{
                 $data = http_build_query($data);
-                $this->headers[] = "Content-Type: application/x-www-form-urlencoded";
+                $this->headers["Content-Type"] = "application/x-www-form-urlencoded";
             }
         }
 
-        $this->headers[] = "Content-Length: ".strlen($data);
+        $this->headers["Content-Length"] = strlen($data);
         //curl_setopt($this->c, CURLOPT_POSTFIELDS, $data);
 
         return $this;
