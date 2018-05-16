e46d589ab35c70de66bc8bb428e0787316083586
karl <fares_k@etna-alternance.net>
2018-05-16 15:44:25 +0100

    update

--- php/html/index.php
+++ php/html/index.php
@@ -1,6 +1,8 @@
 <?php
 
 $aCities = ApiMgr::list('cities');
+var_dump($aCities);
 $aCities = ApiMgr::get('cities', 'ca3e834d-c717-4832-ab8b-c50ebd1bd3d6');
+var_dump($aCities);
 var_dump("END");
 exit;
