73c1a4d934464a83f81d4c896b1f47eda01ad0fc
karl <fares_k@etna-alternance.net>
2018-05-18 12:06:53 +0100

    dev

--- templates/js/html.js
+++ templates/js/html.js
@@ -19,6 +19,8 @@ $.scrollElementVisible = function (el) {
     var elemTop = el.offset().top;
     var elemBottom = elemTop + el.height();
 
+    console.log((elemTop +">="+ docViewTop), (elemTop >= docViewTop));
+    console.log((elemBottom +"<="+ docViewBottom), (elemBottom <= docViewBottom));
 
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
