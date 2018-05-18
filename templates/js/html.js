952858e58a425d122fa64e61d79535f8063ed0f3
karl <fares_k@etna-alternance.net>
2018-05-18 09:50:15 +0100

    dev

--- templates/js/html.js
+++ templates/js/html.js
@@ -21,3 +21,42 @@ $.scrollElementVisible = function (el) {
 
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
+
+
+
+let currentItem = '';
+
+
+
+const fCloseMenuBtn = function fCloseMenuBtn (event) {
+  console.log(event);
+  document.querySelector('.leftBar').classList.toggle('close');
+}
+
+/**
+  * Handle action menu.
+  */
+document.querySelector('#closeMenuBtn')
+  .addEventListener('click', fCloseMenuBtn);
+
+/**
+  * On Sub item click.
+  */
+
+const handlerItemClick = function handlerItemClick (item) {
+  item.addEventListener('click', function (event) {
+    const target = event.currentTarget.dataset.target;
+    const _res = document.querySelector('.item[data-target="'+target+'"]');
+
+    document.querySelector('.item.selected').classList.remove('selected');
+    currentItem = target;
+    _res.classList.toggle('selected');
+  });
+
+}
+
+const itemClick = document.querySelectorAll('.itemClick.withSubItem');
+
+itemClick.forEach(handlerItemClick);
+
+
