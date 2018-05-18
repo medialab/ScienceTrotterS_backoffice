0b48f93b6d9e312abaeed861557bf9f8228d0adb
karl <fares_k@etna-alternance.net>
2018-05-18 10:03:10 +0100

    dev

--- templates/js/html/index.js
+++ templates/js/html/index.js
@@ -1,5 +1,8 @@
-var lists = $('ul.itemList');
+$(document).ready(function() {
+	var lists = $('ul.itemList');
+	console.log("LISTS: ", lists);
 
-lists.scroll(function() {
-  console.log("scrolled");
+	lists.scroll(function() {
+	  console.log("scrolled");
+	})
 })
