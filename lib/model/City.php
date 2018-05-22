7e400b23ed792474525a06453600bd3757aac783
karl <fares_k@etna-alternance.net>
2018-05-22 16:20:18 +0100

    dev: 22/05/18

--- lib/model/City.php
+++ lib/model/City.php
@@ -42,7 +42,7 @@ class City extends Model
 	}
 
 	function __set($sVar, $var) {
-		var_dump("City Update: $sVar");
+		var_dump("City Update: $sVar", $var);
 		if ($sVar === 'geoloc') {
 			$this->setGeoloc($var);
 		}
