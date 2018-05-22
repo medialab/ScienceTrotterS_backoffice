f3cfa522702dc3538630c2229e8766a2438ce239
karl <fares_k@etna-alternance.net>
2018-05-22 16:07:19 +0100

    dev: 22/05/18

--- lib/model/City.php
+++ lib/model/City.php
@@ -19,24 +19,36 @@ class City extends Model
 		Parent::__construct($id, $aData);
 	}
 
-	function __set($sVar, $var) {
-		if ($sVar === 'geoloc') {
-			if (empty($var)) {
-				return;
-			}
+	public function load($aData) {
+		Parent::load($aData);
+		$this->setGeoloc($this->geoloc);
+	}
 
-			$aMatches = [];
-			if (!preg_match_all('/^([0-9]{2}\.[0-9]{4});([0-9]{2}\.[0-9]{4})$/', $geoloc, $aMatches)) {
-				trigger_error("Faild to Set City::geoloc propoerty. Value is Invalid");
-				return;
-			}
-
-			var_dump($aMatches);
-			exit;
+	public function setGeoloc($geoloc) {
+		if (empty($geoloc)) {
 			$this->geoloc = $geoloc;
 			$this->geoloc = $geoloc;
-
 			return;
+		}
+
+		$aMatches = [];
+		if (!preg_match_all('/^([0-9]{2}\.[0-9]{4});([0-9]{2}\.[0-9]{4})$/', $geoloc, $aMatches)) {
+			trigger_error("Faild to Set City::geoloc propoerty. Value is Invalid");
+			return;
+		}
+
+		var_dump($aMatches);
+		exit;
+		$this->geoloc = $geoloc;
+		$this->geoloc = $geoloc;
+
+		return;
+	}
+
+	function __set($sVar, $var) {
+		var_dump("SETTING CITY VAR", $sVar, $var);
+		if ($sVar === 'geoloc') {
+			$this->setGeoloc($var);
 		}
 
 		Parent::_set($sVar, $var);
