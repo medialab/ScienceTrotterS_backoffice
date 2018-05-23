32f430825bb771977e6bbe6f000d5e5e2cd714d2
karl <fares_k@etna-alternance.net>
2018-05-23 12:16:35 +0100

    dev: 23/05/18

--- lib/functions.php
+++ lib/functions.php
@@ -1,4 +1,27 @@
 <?php
+// Remplacement des caractères spéciaux
+	function fRemoveSpecialChar( $sString, $sChar="-" ) {
+		$fRemoveSpecialChar					=	$sString;
+		$fRemoveSpecialChar					=	preg_replace( '`(\\r|\\n|\\t|\/\*(.+)\*\/)`Us', '', $fRemoveSpecialChar );
+		$fRemoveSpecialChar					=	preg_replace( '`(\ +)`', ' ', $fRemoveSpecialChar );
+		$fRemoveSpecialChar					=	preg_replace( '`(\’|\=|\^|\%|\$|\+|\-|\*|_|\@|\&|\(|\)|\!|\[|\]|\#|\ |\,|\.|\;|\/|\'|\:|\°|\?|\"|\\\\|\®|\™)`', $sChar, $fRemoveSpecialChar );
+		$fRemoveSpecialChar					=	preg_replace( '`(\\{$sChar}+)`', $sChar, $fRemoveSpecialChar );
+		$fRemoveSpecialChar					=	preg_replace( '`({$sChar}+)`', $sChar, $fRemoveSpecialChar );
+		$fRemoveSpecialChar					=	trim( $fRemoveSpecialChar, $sChar );
+		return $fRemoveSpecialChar;
+	}
+//---
+
+// Suppression des accents
+	function fRemoveAccents( $sString ) {
+		$aChars					=	array(
+			'Š'=>'S', 'š'=>'s', 'Ð'=>'D', 'd'=>'d', 'Ž'=>'Z', 'ž'=>'z', 'C'=>'C', 'c'=>'c', 'C'=>'C', 'c'=>'c','À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E','Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O','Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e','ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>"i", "ï"=>"i", "ð"=>"o", "ñ"=>"n", "ò"=>"o", "ó"=>"o","ô"=>"o", "õ"=>"o", "ö"=>"o", "ø"=>"o", "ù"=>"u", "ú"=>"u", "û"=>"u", "ý"=>"y", "ý"=>"y", "þ"=>"b","ÿ"=>"y", "R"=>"R", "r"=>"r"
+		);
+		return strtr( ($sString), $aChars );
+	}
+//---
+
+
 // Création d'une chaine de caractère URL Friendly ( sans accents, ni caractère spécial, en minuscule )
 	function fCreateFriendlyUrl( $sString ) {
 		return strtolower( fRemoveAccents( fRemoveSpecialChar( $sString ) ) );
