f1fe5f616f66392675647614bbfe88cadb3e619a
karl <fares_k@etna-alternance.net>
2018-05-23 09:38:04 +0100

    dev: 23/05/18

--- php/html/index.php
+++ php/html/index.php
@@ -1,13 +1,13 @@
 <?php
 
-$aCities = ApiMgr::list('cities', true, 5);
+$aCities = ApiMgr::list('cities', false, 5);
 $smarty->assign('aCities', $aCities->data);
 
 
 
-$aParcours = ApiMgr::list('parcours', true, 5);
+$aParcours = ApiMgr::list('parcours', false, 5);
 $smarty->assign('aParcours', $aParcours->data);
 
 
-$aInterrests = ApiMgr::list('interrests', true, 5);
+$aInterrests = ApiMgr::list('interrests', false, 5);
 $smarty->assign('aInterrests', $aInterrests->data);
