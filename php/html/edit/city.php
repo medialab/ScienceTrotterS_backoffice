5504e2f69e99b16ab24725b19b92eb360bdac3c0
karl <fares_k@etna-alternance.net>
2018-05-23 17:41:03 +0100

    dev: 23/05/18

--- php/html/edit/city.php
+++ php/html/edit/city.php
@@ -24,6 +24,7 @@ if (fMethodIs('post')) {
 
 	/* Validation Du Status */
 		$_POST['state'] = (bool) (empty($_POST['state']) ? 0 : $_POST['state']);
+		var_dump($_POST);
 
 	/* Validation De la Géolocalisation */
 		/* Validation De la Latitude */
