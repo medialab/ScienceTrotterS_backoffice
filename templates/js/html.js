3758aff0c2c466b60e486817ce1d47585990d401
karl <fares_k@etna-alternance.net>
2018-05-23 14:41:06 +0100

    dev: 23/05/18

--- templates/js/html.js
+++ templates/js/html.js
@@ -107,6 +107,7 @@ var ApiMgr = {
 			jsonpCallback: 'ApiResponse',
 
 			beforeSend: function(xhr) {
+				console.log("TEST: ".this.apiToken);
 		        xhr.setRequestHeader('Authorization', this.apiToken);
 		        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
 		    },
