01b060c88c95a445922d51dc43f40543ebe4b88f
karl <fares_k@etna-alternance.net>
2018-05-29 11:49:47 +0100

    dev 29/05/18

--- lib/model/City.php
+++ lib/model/City.php
@@ -101,7 +101,7 @@ class City extends Model
 		Parent::__set($sVar, $var);
 	}
 
-	public static function list($limit=0, $page=0, $sClass=) {
+	public static function list($limit=0, $page=0, $sClass=false) {
 		return Parent::list($limit, $page, self::$ssClass);
 	}
 }
