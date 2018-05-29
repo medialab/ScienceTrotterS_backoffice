62b0cf637e55f2502e0bfc979fd184a93cb881cd
karl <fares_k@etna-alternance.net>
2018-05-29 11:19:02 +0100

    dev 29/05/18

--- php/html/index.php
+++ php/html/index.php
@@ -5,6 +5,8 @@ ApiMgr::setLang('fr');
 $aCities = ApiMgr::list('cities', false, 5);
 $smarty->assign('aCities', $aCities->data);
 
+var_dump($aCities);
+
 $aParcours = ApiMgr::list('parcours', false, 5);
 $smarty->assign('aParcours', $aParcours->data);
 
