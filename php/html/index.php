0893a078106e6e2e9b8aa5fb7155015d4af990e1
karl <fares_k@etna-alternance.net>
2018-05-30 09:18:00 +0100

    dev 30/05/18

--- php/html/index.php
+++ php/html/index.php
@@ -8,7 +8,5 @@ $smarty->assign('aCities', $aCities);
 $aParcours = Model\Parcours::list(5);
 $smarty->assign('aParcours', $aParcours);
 
-ApiMgr::$debugMode = true;
 $aInterrests = ApiMgr::list('interrests', false, 5);
-exit;
 $smarty->assign('aInterrests', $aInterrests->data);
