ca5bf72d6de7bd464fc687f94a4919584caedce1
Paul Girard <paul.girard@sciencespo.fr>
2018-06-12 10:53:37 +0200

    conf variables as env variables

--- config/defines.php
+++ config/defines.php
@@ -1,4 +1,4 @@
 <?php
 
-define("API_SSL", false);	// Forcer la Vérification SSL lors des requêtes APÏ
-define("API_URL", 'http://api-sts-stable.actu.com'); // Url de l'API
+define("API_SSL", env('API_SSL'));	// Forcer la Vérification SSL lors des requêtes APÏ
+define("API_URL", env('API_URL')); // Url de l'API
