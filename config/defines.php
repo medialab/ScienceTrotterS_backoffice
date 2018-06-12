74802f24a8f47d08c593317edc450e2e6b88ee3e
Paul Girard <paul.girard@sciencespo.fr>
2018-06-12 11:01:40 +0200

    makes API_SSL a boolean

--- config/defines.php
+++ config/defines.php
@@ -1,4 +1,4 @@
 <?php
 
-define("API_SSL", env('API_SSL'));	// Forcer la Vérification SSL lors des requêtes APÏ
+define("API_SSL", env('API_SSL')=='true');	// Forcer la Vérification SSL lors des requêtes APÏ
 define("API_URL", env('API_URL')); // Url de l'API
